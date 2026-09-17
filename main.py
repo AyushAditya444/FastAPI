f99ee34ca24f0031f3fc89319429393580209bda
ayush aditya <ayushaditya444@gmail.com>
2026-09-17 12:50:30 +0530

    teri maa ki choot

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from schemas import PostCreate, PostResponse
 
 app = FastAPI()
 
-@app.get("/")
+post : list[dict]  = [
+    {
+        "title": "title of post 1",
+        "content": "content of post 1",
+        "id": 1
+    },
+    {
+        "title": "title of post 2",
+        "content": "content of post 2",
+        "id": 2
+    }
+] 
+
+@app.get("/", response_class=HTMLResponse, include_in_schema=False)
+@app.get("/posts", response_class=HTMLResponse, include_in_schema=False)
 def home():
-    return {"message": "Hello World"}
+    return f"<h1>{post[0]['title']}</h1>"
+
+@app.get("/api/post")
+def get_post():
+    return post
